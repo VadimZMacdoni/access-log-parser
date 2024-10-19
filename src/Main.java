7db6622387ec735d050b2520d506faf7b4833af9
Vadim Zufarov <zufarvadimov@gmail.com>
2024-10-19 11:15:32 +0300

    added filereader

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-import java.io.File;
+import java.io.*;
 import java.util.Scanner;
 
 public class Main {
@@ -20,6 +20,33 @@ public class Main {
                 System.out.println("Это файл номер "+count);
                 count++;
             }
+
+            try {
+                FileReader fileReader = new FileReader(path);
+                BufferedReader reader = new BufferedReader(fileReader);
+                String line;
+                int countLines = 0;
+                int minLength = reader.readLine().length();
+                int maxLength = reader.readLine().length();
+                while ((line = reader.readLine()) != null) {
+                        int length = line.length();
+                        if(length>1024) throw new RuntimeException("Файл содержит строку длиннее 1024 символов");
+                        if(length<minLength) {
+                            minLength=length;
+                        }
+                        if(length>maxLength) {
+                            maxLength=length;
+                        }
+                        countLines++;
+                }
+                System.out.println("Количество строк: "+countLines);
+                System.out.println("Длина самой длинной строки: "+maxLength);
+                System.out.println("Длина самой короткой строки: "+minLength);
+            }catch (FileNotFoundException ex) {
+                ex.printStackTrace();
+            }catch (IOException ex) {
+                ex.printStackTrace();
+            }
         }
-      }
     }
+}
